87d39e31828ad1e1de5d7d7ce0ab3ba00ff9913c
Francesco Nisi <francesconisi95@gmail.com>
2021-11-08 16:26:56 +0100

    working on  crud update

--- src/components/add-user.js
+++ src/components/add-user.js
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const AddUser = () => {
+  const [newUser, setNewUser] = useState({
+    id: Math.random(),
+    name: "",
+    avatar: "",
+    age: "",
+    job: "",
+    createdAt: "2021-05-21T10:16:36.456Z",
+  });
+
+  const trackName = (event) => {
+    setNewUser({
+      ...newUser,
+      name: event.target.value,
+    });
+  };
+
+  const trackAge = (event) => {
+    setNewUser({
+      ...newUser,
+      age: event.target.value,
+    });
+  };
+
+  const trackJob = (event) => {
+    setNewUser({
+      ...newUser,
+      job: event.target.value,
+    });
+  };
+
+  const updateData = async () => {
+    const response = await axios.post(
+      "https://5db179198087400014d38a73.mockapi.io/api/v1/users",
+      newUser
+    );
+    console.log(response.data);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    updateData();
+  };
+
   return (
     <div>
       <h1>Add user</h1>
+      <form action="/">
+        <label>Name </label>
+        <input onChange={trackName} value={newUser.name} type="text" placeholder="Insert name..." />
+        <label>Age </label>
+        <input onChange={trackAge} type="number" placeholder="Insert age..."/>
+        <label>job </label>
+        <input onChange={trackJob} type="text" placeholder="Insert job..." />
+        <button onClick={submitHandler} type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,53 @@
-import React from "react";
+import axios from "axios";
+import "./home.css";
+import "antd/dist/antd.css";
+import { Layout } from "antd";
+import { DeleteOutlined, EditOutlined  } from '@ant-design/icons';
+import { Button } from "antd";
+import React, { useState, useEffect } from "react";
+const { Header, Content } = Layout;
+
 const Home = () => {
+  const [datas, setDatas] = useState([]);
+
+  const callData = async () => {
+    const response = await axios.get(
+      "https://5db179198087400014d38a73.mockapi.io/api/v1/users"
+    );
+    const responseDatas = response.data;
+    setDatas(responseDatas);
+  };
+
+  useEffect(() => {
+    callData();
+  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
+      <Header className="add-user-container">
+        <h1>Users list</h1>
+      </Header>
+      <Content>
+        <h2>Add User</h2>
+        <Button size="large" type="primary">
+          Create
+        </Button>
+        {datas.map((item, index) => {
+          return (
+            <div className="item-card" key={index}>
+              <div className="item-card__info">
+                <span>Name: {item.name}</span>
+                <span>Age: {item.age}</span>
+                <span>Job: {item.job}</span>
+              </div>
+              <div className="item-card__operations">
+                <Button className="item-card__button" type="primary"><EditOutlined /></Button>
+                <Button className="item-card__button" type="danger"><DeleteOutlined /></Button>
+              </div>
+            </div>
+          );
+        })}
+      </Content>
     </div>
   );
 };
