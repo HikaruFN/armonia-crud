fb2fa254e42763953dcf1281a65622f1281f2ab8
Francesco Nisi <francesconisi95@gmail.com>
2021-11-09 10:56:03 +0100

    working on add-user

--- src/components/add-user.js
+++ src/components/add-user.js
@@ -1,5 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./add-user.css";
+import {
+  Layout,
+  Typography,
+  Card,
+  Space,
+  Input,
+  Button,
+  Checkbox,
+  Form,
+} from "antd";
+const { Title } = Typography;
+const { Header, Content, Sider } = Layout;
 
 const AddUser = () => {
   const [newUser, setNewUser] = useState({
@@ -37,33 +50,57 @@ const AddUser = () => {
       "https://5db179198087400014d38a73.mockapi.io/api/v1/users",
       newUser
     );
-    console.log(response.data);
+    window.location.replace("/");
   };
 
   const submitHandler = (event) => {
-    event.preventDefault();
     updateData();
   };
 
   return (
     <div>
-      <h1>Add user</h1>
-      <form action="/">
-        <label>Name </label>
-        <input
-          onChange={trackName}
-          value={newUser.name}
-          type="text"
-          placeholder="Insert name..."
-        />
-        <label>Age </label>
-        <input onChange={trackAge} type="number" placeholder="Insert age..." />
-        <label>job </label>
-        <input onChange={trackJob} type="text" placeholder="Insert job..." />
-        <button onClick={submitHandler} type="submit">
-          Submit
-        </button>
-      </form>
+      <Layout>
+        <Header className="header-container">
+          <Title type="success">Add user</Title>
+        </Header>
+
+        <Content>
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 8 }}
+            initialValues={{ remember: true }}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[{ required: true, message: "Please input your name!" }]}
+            >
+              <Input onChange={trackName} />
+            </Form.Item>
+            <Form.Item
+              label="Job"
+              name="job"
+              rules={[{ required: true, message: "Please input your job!" }]}
+            >
+              <Input onChange={trackJob} />
+            </Form.Item>
+            <Form.Item
+              label="Age"
+              name="age"
+              rules={[{ required: true, message: "Please input your age!" }]}
+            >
+              <Input onChange={trackAge} />
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button onClick={submitHandler} type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Content>
+      </Layout>
     </div>
   );
 };
